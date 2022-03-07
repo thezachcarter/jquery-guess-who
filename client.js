53a6451e1d8633dcf63d5167f16a2bff29f28d41
Zach Carter <thezachcarter@gmail.com>
2022-03-07 16:57:34 -0600

    project complete

--- client.js
+++ client.js
@@ -6,44 +6,50 @@ let randomNum = 0;
 function readyNow() {
     console.log('jq ready');
     addGuys();
-    randomNum = randomNumber(1,4);
+    randomNum = randomNumber(1, 4);
     console.log(randomNum);
+    $('#header').empty()
     switch (randomNum) {
-        case 1: 
-            $('#header').append('<h1>Click On: Dane!</h1>');    
-        break;
-        case 2: 
-        $('#header').append('<h1>Click On: Michael!</h1>');    
-        break;
-        case 3: 
-            $('#header').append('<h1>Click On: Brandon!</h1>');    
-        break;
-        case 4: 
-        $('#header').append('<h1>Click On: Zach!</h1>');    
-        break;         
+        case 1:
+            $('#header').append('<h1>Click On: Dane!</h1>');
+            break;
+        case 2:
+            $('#header').append('<h1>Click On: Michael!</h1>');
+            break;
+        case 3:
+            $('#header').append('<h1>Click On: Brandon!</h1>');
+            break;
+        case 4:
+            $('#header').append('<h1>Click On: Zach!</h1>');
+            break;
     }
-    $('#main').on('click', '.photos', checkClick);
+    $('.photos').on('click', checkClick);
 }
 //append divs for each person in the people array
 
 function addGuys() {
     let guyNum = 1;
-    for (let guy of people){
+    for (let guy of people) {
         $('#main').append(`
-            <div class="photos" data-guyNum="${guyNum}"><img src="https://github.com/${guy.githubUsername}.png?size=250" alt="Profile image of ${guy.name}">
+            <div data-guy-num="${guyNum}"><img class="photos" src="https://github.com/${guy.githubUsername}.png?size=250" alt="Profile image of ${guy.name}">
             </div>`);
-            guyNum++;
+        guyNum++;
     }
 
 }
 
-function randomNumber(min, max){
+function randomNumber(min, max) {
     return Math.floor(Math.random() * (1 + max - min) + min);
 }
 
-function checkClick (){
+function checkClick() {
     console.log('checking click')
     console.log($(this).closest('div').data('guyNum'));
-    
-    // if ($(this).closest(div).data('guyNum'))
+    let photoNum = ($(this).closest('div').data('guyNum'));
+    if (photoNum === randomNum) {
+        alert('Hooray!')
+    } else {
+        alert('Booooo!')
+    }
+    readyNow();
 }
